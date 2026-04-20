2a650dc751e4b36b502141c6686dc811039eebd5
Sheyiyuan <sheyiyuantan90@qq.com>
2026-04-20 22:13:00 +0800

    fix: 修复 pages 路由问题

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(() => {
   const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
+  const repository = process.env.GITHUB_REPOSITORY ?? ''
+  const repoName = repository.split('/')[1]
+  const pagesBase = repoName ? `/${repoName}/` : '/'
 
   return {
     plugins: [react()],
-    base: isGithubActions ? '/BASpark-react/' : '/',
+    base: isGithubActions ? pagesBase : '/',
   }
 })
